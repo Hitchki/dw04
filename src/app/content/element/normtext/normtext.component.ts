ad0f90138eec4d6d208ebda12d94a6f65bec212e
Franz Irnberger <franz.irnberger@gmail.com>
2016-12-20 08:22:32 +0100

    normtext

--- src/app/content/element/normtext/normtext.component.ts
+++ src/app/content/element/normtext/normtext.component.ts
@@ -1,14 +1,17 @@
 import {Component, OnInit, Input} from '@angular/core';
+import {EditElement} from "../edit-element/edit-element";
 
 @Component({
   selector: 'dw-normtext',
   templateUrl: './normtext.component.html',
   styleUrls: ['./normtext.component.css']
 })
-export class NormtextComponent implements OnInit {
+export class NormtextComponent extends EditElement implements OnInit {
   @Input() dwNodes: any;
 
-  constructor() { }
+  constructor() {
+    super();
+  }
 
   ngOnInit() {
   }
