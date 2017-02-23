01324fd50ff52e858cd033b8b8668ea4055c8ad3
Franz Irnberger <franz.irnberger@gmail.com>
2017-02-23 13:05:00 +0100

    reducer still in app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -35,13 +35,27 @@ import {
 import {StoreDevtoolsModule} from '@ngrx/store-devtools'
 import {EffectsModule} from '@ngrx/effects'
 import {LoadProjectsEffectService} from './shared/store/effects/load-projects-effect.service'
+import {StoreData} from './shared/store/store-data'
+import {UiState} from './shared/store/ui-state'
 
 function storeReducer(
   state: ApplicationState = INITIAL_APPLICATION_STATE,
   action: Action): ApplicationState  {
 
+  return {
+    uiState: uiStateReducer(state.uiState, action),
+    storeData: storeDataReducer(state.storeData, action)
+  }
+}
+
+function uiStateReducer(state, action) : UiState {
+  return {
+    userId: 1,
+  }
+}
+
+function storeDataReducer(state, action) : StoreData {
   switch (action.type) {
-    // case LOAD_USER_PROJECTS_ACTION:
     case USER_PROJECTS_LOADED_ACTION:
       return handleLoadUserProjectsAction(state, action);
 
@@ -50,16 +64,16 @@ function storeReducer(
   }
 }
 
-function handleLoadUserProjectsAction(state: ApplicationState,
-                                action: UserProjectsLoadedAction): ApplicationState {
+
+function handleLoadUserProjectsAction(
+  state: ApplicationState,
+  action: UserProjectsLoadedAction): StoreData {
+
   const projectData = action.payload;
-
-  console.log('porDataaaaaaaa: ', projectData);
-
-  const newState: ApplicationState = Object.assign({}, state);
-  newState.storeData = {projects: projectData};
+  // const newState: ApplicationState = Object.assign({}, state);
+  return {projects: projectData};
   // alert('projects');
-  return newState;
+  // return newState;
 }
 
 @NgModule({
--- src/app/shared/store/ui-state.ts
+++ src/app/shared/store/ui-state.ts
@@ -2,11 +2,11 @@
 
 export interface UiState {
     userId:number;
-    currentThreadId: number;
+    // currentThreadId: number;
 }
 
 
 export const INITIAL_UI_STATE: UiState = {
     userId: undefined,
-    currentThreadId: undefined
+    // currentThreadId: undefined
 };
